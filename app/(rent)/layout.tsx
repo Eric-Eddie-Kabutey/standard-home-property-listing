0d7668ce243d227101e4cad54964ddb57b140c60
touraye <touraye07@gmail.com>
2025-10-22 17:43:37 +0000

    add rent layout

---
+++ app/(rent)/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import Header from "@/components/layout/header/rent/Header";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Standard Home Property Listing",
+  description: "Search properties with ease",
+};
+
+export default function WelcomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >   
+        <Header />        
+        {children}        
+      </body>
+    </html>
+  );
+}
---
+++ app/(rent)/welcome/page.tsx
@@ -0,0 +1,8 @@
+import MultiStepSearchForm from "@/components/form/multi-step-search-form";
+
+export default function WelcomePage() {
+    return (<>
+        {/* Multi step form */}
+        <MultiStepSearchForm />
+    </>)
+}
